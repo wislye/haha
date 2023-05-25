b1cab2413b6f1097d61a16e9f0cb4b0eda1d6bf9
wislye <134590301+wislye@users.noreply.github.com>
2023-05-25 16:46:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Website</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    
+    <?php
+    // PHP code here
+    echo "This is a simple PHP website.";
+    ?>
+    
+    <p>Here is some static content.</p>
+</body>
+</html>
